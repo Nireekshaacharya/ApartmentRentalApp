f2baefeb1d65f9627b5e8bee715b52b8c5f380c8
Nireekshaacharya <acharyaniriksha2@gmail.com>
2023-11-30 14:42:01 +0530

    Nav button

--- app/src/main/java/com/example/ganapa/MainActivity.java
+++ app/src/main/java/com/example/ganapa/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ganapa;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,64 +12,20 @@ import android.widget.Toast;
 import com.example.ganapa.R;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText username, password, repassword,email_1,Phone,Address;
-    Button signup, signin;
-    DBHelper DB;
+    Button b1;
+   ConstraintLayout cy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       b1=findViewById(R.id.button2);
+       b1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(getApplicationContext(), Login_page.class);
+               startActivity(intent);
+           }
+       });
 
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
-        repassword = (EditText) findViewById(R.id.repassword);
-        email_1=findViewById(R.id.email_1);
-        Phone=findViewById(R.id.Phone);
-        Address=findViewById(R.id.Address);
-        signup = (Button) findViewById(R.id.btnsignup);
-        signin = (Button) findViewById(R.id.btnsignin);
-        DB = new DBHelper(this);
-
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String user = username.getText().toString();
-                String pass = password.getText().toString();
-                String em=email_1.getText().toString();
-                String ph=Phone.getText().toString();
-                String ad=Address.getText().toString();
-                String repass = repassword.getText().toString();
-
-                if(user.equals("")||pass.equals("")||repass.equals("")||em.equals("")||ph.equals("")||ad.equals(""))
-                    Toast.makeText(MainActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
-                else{
-                    if(pass.equals(repass)){
-                        Boolean checkuser = DB.checkusername(user, em);
-                        if(checkuser==false){
-                            Boolean insert = DB.insertData(user,pass,em,ph,ad);
-                            if(insert==true){
-                                Toast.makeText(MainActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
-
-                            }else{
-                                Toast.makeText(MainActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        else{
-                            Toast.makeText(MainActivity.this, "User already exists! please sign in", Toast.LENGTH_SHORT).show();
-                        }
-                    }else{
-                        Toast.makeText(MainActivity.this, "Passwords not matching", Toast.LENGTH_SHORT).show();
-                    }
-                } }
-        });
-
-        signin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
