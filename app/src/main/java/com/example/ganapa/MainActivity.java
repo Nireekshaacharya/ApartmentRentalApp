7a3059b4e632b81dc64c36ffa947da3c1ab3779f
Nireekshaacharya <acharyaniriksha2@gmail.com>
2023-12-01 22:48:15 +0530

    Nav button

--- app/src/main/java/com/example/ganapa/MainActivity.java
+++ app/src/main/java/com/example/ganapa/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.ganapa;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
-
+import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,16 +19,27 @@ public class MainActivity extends AppCompatActivity {
   NavigationView navigationView;
   ActionBarDrawerToggle drawerToggle;
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout=findViewById(R.id.drawer_layout);
-        navigationView=findViewById(R.id.nav_view);
-        drawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);  // Reference the Toolbar
+        setSupportActionBar(toolbar);  // Set Toolbar as the support action bar
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -34,12 +47,23 @@ public class MainActivity extends AppCompatActivity {
                 if (itemId == R.id.Home) {
                     Toast.makeText(MainActivity.this, "In home only", Toast.LENGTH_SHORT).show();
                 } else if (itemId == R.id.login) {
-                    Intent intent = new Intent(getApplicationContext(), Login_page.class);
+                    Intent intent = new Intent(MainActivity.this, Login_page.class);
                     startActivity(intent);
+
                 }
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
 
